a3e295e6ada922dc9b59c11710685122fba729f7
jarrah.swavley1 <jarrah.swavley1@600900J211DE402>
2018-04-10 18:01:26 +1000

    Start programming Player script

--- Assets/~GoneHome/Scripts/Player.cs
+++ Assets/~GoneHome/Scripts/Player.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public string message = "Hello Worlds!";
+    public Rigidbody rigid;
+
+    float speed = 5;
 
     // Use this for initialization
     void Start()
     {
-      
+
     }
+    /*
+     * []-Brackets
+     * {}-Braces
+     * ()-Parenthesis
+     * Hot keys
+     * - Clean Code CTRL + K + D
+     * -Fold Code Ctrl + M + O
+     * -Unfold Code CTRL + M + P
+     */
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rigid.AddForce(Vector3.forward * speed);
+        }
 
     }
 }
