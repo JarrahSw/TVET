18e8f303ff3609f6c107570fa597d7c00bdb9db2
jarrah.swavley1 <jarrah.swavley1@600900J211DE402>
2018-05-01 17:49:03 +1000

    Added visual studios project

---
+++ unity/Assets/~GoneHome/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string message = "Hello Worlds!";
+    public Rigidbody rigid;
+
+    float speed = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    /*
+     * []-Brackets
+     * {}-Braces
+     * ()-Parenthesis
+     * Hot keys
+     * - Clean Code CTRL + K + D
+     * -Fold Code Ctrl + M + O
+     * -Unfold Code CTRL + M + P
+     */
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rigid.AddForce(Vector3.forward * speed);
+        }
+
+    }
+}
