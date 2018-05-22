f16f223f7388d3d7123a165da7c2597655597aaa
jarrah.swavley1 <jarrah.swavley1@600900J211DE402>
2018-05-22 17:51:18 +1000

    starts minesweeper

---
+++ unity/Assets/~Minesweeper/Scripts/Tile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Minesweeper
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Tile : MonoBehaviour
+    {
+        public int x, y;
+        public bool isMine = false; // is the current tile a mine?
+        public bool isRevealed = false; //Has the tile already been revealed
+        [Header("References")]
+        public Sprite[] emptySprites; // List of empty sprites i.e, empty, 1, 2, 3, etc...
+        public Sprite[] mineSprites;
+        private SpriteRenderer rend;
+
+        void Awake()
+        {
+            rend = GetComponent<SpriteRenderer>();
+        }
+        void Start()
+        {
+            isMine = Random.value < .05f;
+        }
+
+        public void Reveal(int adjacentMines, int mineState = 0)
+        {
+            isRevealed = true;
+            if(isMine)
+            {
+                rend.sprite = mineSprites[mineState];
+            }
+            else
+            {
+                rend.sprite = emptySprites[adjacentMines];
+            }
+        }
+    }
+}
